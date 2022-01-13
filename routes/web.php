281f4e8fe38d3b8b15f8ec30faa3acb6a784f558
Annikangl <svinpauk78@gmail.com>
2022-01-13 16:41:20 +0300

    work commit

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Route::resource('rest', RestTestController::class);
+
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
+    Route::resource('posts', PostController::class);
+});
+
+// админка блога
+$groupData = [
+    'namespace' => 'App\Http\Controllers\Blog\Admin',
+    'prefix' => 'admin/blog',
+    'as' => 'blog.admin.'
+];
+
+Route::group($groupData, function() {
+    // BlogCategory
+    $methods = ['index','store','create','edit','update'];
+    Route::resource('categories', CategoryController::class)->only($methods);
+});
+
